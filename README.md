7e5002fb630c318291e77d3f12ac03bbcb6c86ab
GDCStoney <g.stoney@btinternet.com>
2020-10-25 17:54:48 +0000

    Readme added

---
+++ README.md
@@ -0,0 +1,13 @@
+# Bank tech test
+
+This is a small self contained piece of code to simulate some of the core
+functionality of a 'bank account'.
+
+The functions are run from a REPL interface like IRB.
+
+Code functions:
+deposit(date, amount) - to create a deposit transaction
+withdraw(date, amount) - to create a withdrawl transaction
+statement_print - to print out a list of transactions in reverse date order
+
+The functionality is to meet the requirements laid out in the Bank_tech_test challenge.
--- lib/bank_tech_test.rb
+++ lib/bank_tech_test.rb
@@ -7,30 +7,39 @@ class Account
   end
 
   def deposit(date, amount)
-    @balance += amount
-    @activity << [DateTime.parse(date.to_s).strftime("%d-%m-%Y"), sprintf('%.2f', amount), nil, sprintf('%.2f', @balance)]
+    @activity << [format_date(date), format_amount(amount), nil, balance(amount)]
   end
 
   def withdraw(date, amount)
-    @balance -= amount
-    @activity << [DateTime.parse(date.to_s).strftime("%d-%m-%Y"), nil, sprintf('%.2f', amount), sprintf('%.2f', @balance)]
+    @activity << [format_date(date), nil, format_amount(amount), balance(-amount)]
   end
 
   def statement_print
     puts "date || credit || debit|| balance"
-    @activity.sort! { |action1, action2| action2[0] <=> action1[0] }
-    @activity.each { |action| puts "#{action.join(' || ')}" }
+    @activity.sort { |action1, action2|
+      action2[0] <=> action1[0]
+    }.each { |action|
+      puts "#{action.join(' || ')}"
+    }
   end
 
   private
+  def format_date(date)
+    DateTime.parse(date.to_s).strftime("%d/%m/%Y")
+  end
+
+  def format_amount(amount)
+    sprintf('%.2f', amount)
+  end
+
   def balance(amount)
-    @balance += amount
+    format_amount(@balance += amount)
   end
 end
 
 account = Account.new
-account.deposit(Time.new(2012, 01, 10).strftime("%d-%m-%Y"), 1000.00)
-account.deposit(Time.new(2012, 01, 13).strftime("%d-%m-%Y"), 2000.00)
-account.withdraw(Time.new(2012, 01, 14), 500.00)
+account.deposit("10-01-2012", 1000.00)
+account.deposit("13-01-2012", 2000.00)
+account.withdraw("14-01-2012", 500.00)
 
 account.statement_print
